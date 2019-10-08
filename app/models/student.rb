f68ea888579fbae2c35a535857b0298490bd6c57
Akash Pateria <pateria.akash77@gmail.com>
2019-10-08 19:34:48 -0400

    add student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,4 +10,22 @@ class Student < ApplicationRecord
   has_many :borrowing_histories, dependent: :destroy    
   has_many :book_hold_requests, dependent: :destroy    
 
+
+  def self.calculate_overdue_fine(student_id) 
+    overdue_fine = 0 
+    checkout_books = BorrowingHistory.fetch_all_active_books_by_student(student_id)
+
+    checkout_books.map { |checkout_book|
+      library_id = BookLibraryMapping.fetch_library_for_book(checkout_books.book_id)
+
+      if not library_id.nil?
+        library = Library.find(library_id)
+        max_days_blocking = library.max_days_blocking 
+        overdue_fine += ((Date.today - checkout_book.issue_date).abs + 1) * ibrary.overdue_fines
+      end
+    }
+
+    overdue_fine
+  end
+  
 end
